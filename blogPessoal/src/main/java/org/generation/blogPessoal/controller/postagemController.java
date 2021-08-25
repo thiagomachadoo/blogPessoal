2cf2c90bc084c33ccac7ac06a1e14ec24ae512a9
thiagomachadoo <thiagodasilvamachadoo44@gmail.com>
2021-08-25 12:17:51 -0300

    incrementando o findById

--- blogPessoal/src/main/java/org/generation/blogPessoal/controller/postagemController.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/controller/postagemController.java
@@ -4,10 +4,7 @@ import org.generation.blogPessoal.PostagemRepository;
 import org.generation.blogPessoal.model.Postagem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +19,11 @@ public class postagemController {
     public ResponseEntity<List<Postagem>> getAll(){
         return ResponseEntity.ok(repository.findAll());
     }
+    @GetMapping("/{id}")
+    public ResponseEntity<Postagem> GetById(@PathVariable long id){
+        return repository.findById(id)
+                .map(resp -> ResponseEntity.ok(resp))
+                .orElse(ResponseEntity.notFound().build());
 
+    }
 }
