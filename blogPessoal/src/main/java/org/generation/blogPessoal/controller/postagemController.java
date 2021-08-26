048b0d3468e2909d6989abec425da7fd7b83abde
thiagomachadoo <thiagodasilvamachadoo44@gmail.com>
2021-08-25 21:05:32 -0300

    Aplicando Get, Put, Post e fazendo testes na api

--- blogPessoal/src/main/java/org/generation/blogPessoal/controller/postagemController.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/controller/postagemController.java
@@ -3,9 +3,10 @@ package org.generation.blogPessoal.controller;
 import org.generation.blogPessoal.PostagemRepository;
 import org.generation.blogPessoal.model.Postagem;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import java.lang.Long;
 import java.util.List;
 
 @RestController
@@ -21,10 +22,22 @@ public class postagemController {
     }
     @GetMapping("/{id}")
     public ResponseEntity<Postagem> GetById(@PathVariable long id){
-        return repository.findById(id)
-
-                .map(resp -> ResponseEntity.ok(resp))
-                .orElse(ResponseEntity.notFound().build());
-
+        return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+    }
+    @GetMapping("/titulo/{titulo}")
+    public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo){
+        return  ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+    }
+    @PostMapping //INSERÇÃO
+    public ResponseEntity<Postagem> post (@RequestBody Postagem postagem){
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+    }
+    @PutMapping //ATUALIZAÇÃO
+    public ResponseEntity<Postagem> put (@RequestBody Postagem postagem){
+        return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+    }
+    @DeleteMapping("/{id}") //
+    public void delete(@PathVariable long id) {
+        repository.deleteById(id);
     }
 }
