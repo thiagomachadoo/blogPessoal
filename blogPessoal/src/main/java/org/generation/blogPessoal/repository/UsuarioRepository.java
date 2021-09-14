32e7a305b52253bf772f32c4079020db836857ba
Thiago Machado <thiagodasilvamachadoo44@gmail.com>
2021-09-13 21:12:19 -0300

    implementando deploy no blog e fazendo testes unitários

--- blogPessoal/src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
@@ -1,10 +1,16 @@
 package org.generation.blogPessoal.repository;
 
-import org.generation.blogPessoal.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 import java.util.Optional;
 
+import org.generation.blogPessoal.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    public Optional<Usuario> findByUsuario(String usuario);
+	public Optional<Usuario> findByUsuario(String usuario);
+
+	// Usaremos na Sessão de Testes
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
+
+	// Usaremos na Sessão de Testes
+	public Usuario findByNome(String nome);
 }
